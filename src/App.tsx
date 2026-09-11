1383c0c2e158ddb20357bf7b154424c9317c9079
ivan.bomko <ivan.bomko@epita.fr>
2026-09-11 13:50:27 +0200

    use state and filter integration into a searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import MovieList from "./components/MovieList";
 import SearchBar from "./components/SearchBar";
 import { SAMPLE_MOVIES } from "./data/sampleMovies";
+import { useState } from  "react"; 
 
 const App = () => {
+  const [query, setQuery] = useState("");
+
+  const filteredMovies = SAMPLE_MOVIES.filter((m) =>
+    m.title.toLowerCase().includes(query.toLowerCase()),
+  );
+
   return (
     <div className="app-layout">
       <main className="main-container">
         <h1>Movie App</h1>
-        <SearchBar />
-        <MovieList movies={SAMPLE_MOVIES} />
+        <SearchBar query={query} onChange={setQuery} />
+        <MovieList movies={filteredMovies} />
       </main>
     </div>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,36 @@
-const SearchBar = () => {
-    return <div>SearchBar</div>;
+
+
+export type SearchBarProps = {
+    query: string;
+    onChange: (newQuery: string) => void;
+};
+
+const SearchBar = ({ query, onChange }: SearchBarProps) => {
+    return (
+        <div className="header-search">
+            <div className="header-search">
+                <div className="seach-input-wrapper">
+                    <svg className="search-icon"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                    <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search movies, plot keywords, titles..."
+                    autoComplete="off"
+                    onChange={(e) => onChange(e.target.value)}
+                    value={query}
+                    ></input>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default SearchBar;
